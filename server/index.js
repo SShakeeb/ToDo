ac2471eaba2fce8e53b7bc3a93abd7398c916fb7
SShakeeb <shakeebshirazi@gmail.com>
2022-06-19 19:29:48 +0300

    Add update route & todo.json as []

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require("express"); // require express framework
+const bodyParser = require('body-parser'); // body parser for patch req
 const PORT = process.env.PORT || 3001;
 const todoRoutes = require("./routes/todoRoutes");
 const app = express();
+
+app.use(bodyParser.json())
 
 app.use("/api/todoRoutes", todoRoutes)
 
--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -2,7 +2,7 @@ const express = require("express"); // require express framework
 const router = express.Router();
 const FILEPATH = "./models/todo.json";
 const readJson = require("../domains/jsonReader");
-//const writeJson = require("../domains/jsonWriter");
+const writeJson = require("../domains/jsonWriter");
 
 // An endpoint to get all list of todo
 router.get('/', function(req, res){
@@ -16,11 +16,36 @@ router.get('/', function(req, res){
             res.status(200).send(todo);
         }
         catch (err) {
-            return res.status(401).send(err); 
+            return res.status(500).send(err); 
         }
-        
-        // To write todo JSON file
-        // writeJson(FILEPATH,todo);
     });
 })
+
+// An endpoint to update status based on ID
+router.patch('/:id', (req, res) => {
+    // First to read todo.json file
+    readJson(FILEPATH, (err, todo) => {
+        if (err){
+            console.log(err)
+            return
+        }
+        try {
+            //Find index of specific object using findIndex method.    
+            objIndex = todo.findIndex((obj => obj.id == req.params.id));
+            if (objIndex){
+                return res.status(404).send("Todo not found");
+            }
+            //Update object's status property.
+            todo[objIndex].status = req.body.status;
+            // To write updated todo to JSON file
+            writeJson(FILEPATH,todo);
+            return res.status(200).send(todo[objIndex]);
+            
+        }
+        catch (err) {
+            return res.status(500).send(err); 
+        }
+  });
+})
+
 module.exports = router;
